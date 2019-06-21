9841ab1ed9e7379ee752fbcb6cadad38d477374f
Shannon Wotherspoon <39409709+shwotherspoon@users.noreply.github.com>
2019-06-21 18:01:03 -0400

    Fix bug in handling strs of different length

--- min_edit_dist.py
+++ min_edit_dist.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-def compute_med(s1, s2, ins_cost=1, del_cost=1, sub_cost=2):
+
+def compute_med(s1, s2, ins_cost=1, del_cost=1, sub_cost=1):
 	'''
 	Compute the minimum edit distance (MED) between string 1 (s1) and
 	string 2 (s2)
 	'''
-	target_len = len(s1)+1 # num rows
-	source_len = len(s2)+1 # num cols
-	matr = np.zeros((source_len,target_len))
+	target = s1 
+	source = s2
+	target_len = len(target)+1 # num cols
+	source_len = len(source)+1 # num rows
+	matr = np.zeros((source_len, target_len))
 	matr[0,0] = 0 
 
 	# initialize first row and first column
@@ -15,14 +18,15 @@ def compute_med(s1, s2, ins_cost=1, del_cost=1, sub_cost=2):
 		matr[row,0] = row
 	for col in range(matr.shape[1]): 
 		matr[0,col] = col 
-	
+	print(matr)
 
 	# fill in the rest of the table
-	for row in range(1,source_len):
+	for row in range(1,source_len): 
 		for col in range(1,target_len):
+			print(row, col)
 			ins_dist = matr[row, col-1] + ins_cost
 			del_dist = matr[row-1, col] + del_cost
-			if s1[row-1] == s2[col-1]:
+			if source[row-1] == target[col-1]:
 				sub_dist = matr[row-1, col-1]
 			else:
 				sub_dist = matr[row-1, col-1] + sub_cost
@@ -30,13 +34,13 @@ def compute_med(s1, s2, ins_cost=1, del_cost=1, sub_cost=2):
 			matr[row,col] = dist
 
 	print(matr)
-	med = matr[target_len-1, source_len-1]
+	med = matr[source_len-1, target_len-1]
 	return med 
 
 
 def main():
-	s1 = 'intention'
-	s2 = 'execution'
+	s1 = 'honda'
+	s2 = 'hyundai'
 	med = compute_med(s1, s2)
 	print(med)
 
